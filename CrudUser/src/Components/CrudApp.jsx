f4d88cbc238a33232f5d86a9ef979adaf9476c87
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-05-09 17:00:13 -0600

    CrudApp.jsx

--- CrudUser/src/Components/CrudApp.jsx
+++ CrudUser/src/Components/CrudApp.jsx
@@ -93,7 +93,7 @@ const CrudApp = () => {
 
   return (
     <div>
-      {/* <h2>Name</h2> */}
+      <h2>Luis Villa y Ronaldo Suarez</h2>
       <CrudForm
         creatData={creatData}
         updateData={updateData}
