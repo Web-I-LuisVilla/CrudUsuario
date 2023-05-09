bcaea574f1e82f538a3c41265a0f9c9da5c50dcf
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-05-09 18:01:39 -0500

    Final

--- CrudUser/src/App.jsx
+++ CrudUser/src/App.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import CrudApp from "./Components/CrudApp";
-// import ContactForm from "./components/ContactForm";
-// import CrudApi from "./components/CrudApi";
-// import Modals from "./components/Modals";
-// import SelectsAnidados from "./components/SelectsAnidados";
-// import SongSearch from "./components/SongSearch";
-
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-//  import 'bootstrap/dist/css/bootstrap.min.css'
-//  import {Table,Button,Container,Modal,ModalHeader,ModalBody,FormGroup,ModalFooter,} from "reactstrap";
 
 function App() {
   return (
